e9c438d7617d0f5445fd8dcfaf389f520f98d170
Yulchikss <yuliapelia19@gmail.com>
2025-01-03 16:59:45 +0200

    added new integration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,11 +6,12 @@ import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import type { Metadata } from 'next';
 import { Montserrat, Inter } from "next/font/google";
+import { getTranslations } from 'next-intl/server';
 
-// type Props = {
-//   params: Promise<{ locale: string }>
-//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-// }
+type Props = {
+  params: Promise<{ locale: string }>
+ 
+}
 type Locale = 'en' | 'ua' ;
 
 
@@ -24,32 +25,41 @@ const montserrat = Montserrat({
 });
 
 
-// export async function generateMetadata({ params }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata>  {
+export async function generateMetadata({ params }: Props,
+ 
+): Promise<Metadata>  {
 
-//   const locale = (await params).locale
-//   console.log('id',locale);
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+  console.log('id',locale);
   
-//   const previousImages = (await parent).openGraph?.images || []
+  // const previousImages = (await parent).openGraph?.images || []
 
-//   console.log('previousImages',previousImages);
+  // console.log('previousImages',previousImages);
   
   
 
-//   return {
-//     title: '...',
-//     metadataBase: new URL("http://localhost:3000/"),
-//     openGraph: {
-//       images: ['/image/OGP/fb.png', ...previousImages],
-//     },
-//   }
-// }
+  return {
+    title: t('title'),
+    description: t('description'),
+    icons: {
+      icon: '/image/favicon_io/favicon-16x16.png', 
+      apple: '/image/favicon_io/apple-touch-icon.png', 
+    },
+    manifest: '/image/favicon_io/site.webmanifest', 
+    // metadataBase: new URL(`https://portfolio-black-kappa-60.vercel.app/${locale}/`),
+    // openGraph: {
+    //   images: ['/image/OGP/fb.png'],
+    // },
+  }
+}
 
-export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "My portfolio",
-};
+
+
+// export const metadata: Metadata = {
+//   title: "Portfolio",
+//   description: "My portfolio",
+// };
 export default async function LocaleLayout({
   children,
   params
