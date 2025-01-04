28c3e60e15f1b83946ea34f6820b46b90061f9a1
Yulchikss <yuliapelia19@gmail.com>
2025-01-04 23:34:15 +0200

    test for OGP

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -53,12 +53,12 @@ export async function generateMetadata({ params }: Props,
       siteName: 'Yulia Pelyachyk',
 
       images: [{
-        url: t('ogpFb'),
+        url: locale === 'en' ? '/image/OGP/fb-en.png' : '/image/OGP/fb.png',
         width: 1200,
         height: 630
       },
     {
-      url: t('ogpTw'),
+      url: locale === 'en' ? '/image/OGP/tw-en.png' : '/image/OGP/tw.png',
       width: 800,
       height: 600
     }],
